0873282896bdc6bbef3b03740ba3b65963b91328
Will <w.marino997@gmail.com>
2020-01-19 18:46:09 -0500

    changing game reset

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import "./styles/index.scss";
 import Game from './game';
 
 const canvas = document.getElementById('main-canvas');
-const game = new Game(canvas);
 
 document.addEventListener('DOMContentLoaded', () => {
+  let game = new Game(canvas);
   game.restart();
 });
+
+// let playButton = document.getElementById('play-button');
+
+// playButton.addEventListener('mousedown', () => {
+//   let game = new Game(canvas);
+//   game.restart();
+// })
 
